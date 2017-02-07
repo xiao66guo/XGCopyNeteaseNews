51a9f9ae3a9e78623ac588f78792c9895c752b16
xiao66guo <weixiaoguo@xiao66guo.local>
2017-02-07 22:37:27 +0800

    设置新闻接口的方法

--- CopyNeteaseNews/CopyNeteaseNews/Tools/Network/XGNetworkManager.h
+++ CopyNeteaseNews/CopyNeteaseNews/Tools/Network/XGNetworkManager.h
@@ -12,4 +12,14 @@
 
 + (instancetype)shareManager;
 
+/**
+ *  加载新闻列表
+ *
+ *  @param channel    频道的字符串
+ *  @param start      开始加载时的数字
+ *  @param completion 加载完成时的回调
+ *
+ */
+- (void)newsListWithChannel:(NSString *)channel start:(NSInteger)start completion:(void (^)(NSArray *array, NSError *error))completion;
+
 @end
