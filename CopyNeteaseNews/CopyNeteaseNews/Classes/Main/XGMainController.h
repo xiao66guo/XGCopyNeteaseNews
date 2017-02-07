03ecf3cf1deb0801e7118192caaf7ab4f1dffc98
一个老兵 <xiao66guo@163.com>
2017-02-07 19:04:50 +0800

    初始化控制器、添加 Pod 框架、设置 gitignore 文件
    初始化控制器、添加 Pod 框架、设置 gitignore 文件

---
+++ CopyNeteaseNews/CopyNeteaseNews/Classes/Main/XGMainController.h
@@ -0,0 +1,13 @@
+//
+//  XGMainController.h
+//  CopyNeteaseNews
+//
+//  Created by 小果 on 2017/2/7.
+//  Copyright © 2017年 小果. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XGMainController : UITabBarController
+
+@end
---
+++ CopyNeteaseNews/CopyNeteaseNews/Classes/Main/XGMainController.m
@@ -0,0 +1,37 @@
+//
+//  XGMainController.m
+//  CopyNeteaseNews
+//
+//  Created by 小果 on 2017/2/7.
+//  Copyright © 2017年 小果. All rights reserved.
+//
+
+#import "XGMainController.h"
+
+@interface XGMainController ()
+
+@end
+
+@implementation XGMainController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
