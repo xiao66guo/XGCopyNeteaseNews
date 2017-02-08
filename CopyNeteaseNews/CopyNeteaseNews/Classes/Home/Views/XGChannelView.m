ddb843d461a844126ae5ea9107417b4de31cff49
xiao66guo <weixiaoguo@xiao66guo.local>
2017-02-08 17:08:56 +0800

    设置频道标签中的布局

--- CopyNeteaseNews/CopyNeteaseNews/Classes/Home/Views/XGChannelView.m
+++ CopyNeteaseNews/CopyNeteaseNews/Classes/Home/Views/XGChannelView.m
@@ -27,12 +27,27 @@
 - (void)setChannelList:(NSArray<XGChannel *> *)channelList {
     _channelList = channelList;
     
+    // 初始化 lab 的数据
+    CGFloat x = 20;
+    CGFloat margin = 8;
+    CGFloat height = _scrollView.bounds.size.height;
+    
     // 向 scrollView 中添加频道的控件
     for (XGChannel *channel in channelList) {
         // 频道标签
         XGChannelLabel *channelLab = [XGChannelLabel channelLabelWithTitle:channel.tname];
         
+        // 设置label的位置
+        channelLab.frame = CGRectMake(x, 0, channelLab.bounds.size.width, height);
+        // 用递增的方法设置其他label的位置
+        x += channelLab.bounds.size.width + margin;
+        
         [_scrollView addSubview:channelLab];
+        
+        // 设置 scrollView 滚动的条件
+        _scrollView.contentSize = CGSizeMake(x, height);
+        _scrollView.showsVerticalScrollIndicator = NO;
+        _scrollView.showsHorizontalScrollIndicator = NO;
     }
 }
 @end
