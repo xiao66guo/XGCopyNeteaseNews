77c99928a98cc7d087d63bde1781c5bb233bd4d2
xiao66guo <weixiaoguo@xiao66guo.local>
2017-02-08 22:08:05 +0800

    加载body的内容并遍历 img 数组，替换图片

--- CopyNeteaseNews/CopyNeteaseNews/Classes/Home/NewsDetail/XGNewsDetailController.m
+++ CopyNeteaseNews/CopyNeteaseNews/Classes/Home/NewsDetail/XGNewsDetailController.m
@@ -29,7 +29,31 @@
 
 - (void)loadData {
     [[XGNetworkManager shareManager] newsDetailWithDocid:_detailItem.docid completion:^(NSDictionary *dict, NSError *error) {
-        NSLog(@"%@",dict);
+        
+        NSString *body = dict[@"body"];
+        NSArray *img = dict[@"img"];
+        NSArray *video = dict[@"video"];
+        
+        NSLog(@"=====%@",body);
+        NSLog(@"%@",img);NSLog(@"******%@",video);
+        
+        // 追加图片数据
+        // 循环遍历 img 的数组，查找 body 中 ref 的位置，如果找到使用 src 替换对应的图片内容
+        for (NSDictionary *dict in img) {
+            // 取出 ref 的内容
+            NSString *ref = dict[@"ref"];
+            // 在body中查找出 ref 的位置
+            NSRange range = [body rangeOfString:ref];
+            // 判断是否找到
+            if (range.location == NSNotFound) {
+                continue;
+            }
+            // 替换 body 中range 对应的内容
+            NSString *imgStr = [NSString stringWithFormat:@"<img src=\"%@\" />",dict[@"src"]];
+            body = [body stringByReplacingCharactersInRange:range withString:imgStr];
+        }
+        
+        [self.webView loadHTMLString:body baseURL:nil];
     }];
 }
 @end
